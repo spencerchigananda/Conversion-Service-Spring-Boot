6b92ef3ab07238fba7d6e8f435c47de809c07e12
Spencer Chigananda <schigananda@bancabc.co.zw>
2021-01-20 16:34:15 +0200

    Create constants class to hold conversion constants

---
+++ src/main/java/com/spencerchigananda/conversionservice/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.spencerchigananda.conversionservice.utils;
+
+import java.math.BigDecimal;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final BigDecimal ABSOLUTE_ZERO_CONSTANT = BigDecimal.valueOf(273.15);
+    public static final BigDecimal MILES_TO_KM_CONSTANT = BigDecimal.valueOf(1.609);
+}
