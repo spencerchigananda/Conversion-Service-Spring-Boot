75c753fee9ae9a6a961b4f48d464d916373a971d
Spencer Chigananda <schigananda@bancabc.co.zw>
2021-01-20 16:40:12 +0200

    Implemented conversions in service layer using constants

--- src/main/java/com/spencerchigananda/conversionservice/services/ConversionServiceImpl.java
+++ src/main/java/com/spencerchigananda/conversionservice/services/ConversionServiceImpl.java
@@ -1,8 +1,10 @@
 package com.spencerchigananda.conversionservice.services;
 
+import com.spencerchigananda.conversionservice.utils.Constants;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class ConversionServiceImpl implements ConversionService{
@@ -10,21 +12,25 @@ public class ConversionServiceImpl implements ConversionService{
 
     @Override
     public BigDecimal kelvinToCelsius(BigDecimal kelvin) {
-        return null;
+        // C = K - 273.15
+        return kelvin.subtract(Constants.ABSOLUTE_ZERO_CONSTANT);
     }
 
     @Override
     public BigDecimal celsiusToKelvin(BigDecimal celsius) {
-        return null;
+        // K = C + 273.15
+        return celsius.add(Constants.ABSOLUTE_ZERO_CONSTANT);
     }
 
     @Override
     public BigDecimal milesToKilometres(BigDecimal miles) {
-        return null;
+        // Km = Miles * 1.609
+        return miles.multiply(Constants.MILES_TO_KM_CONSTANT);
     }
 
     @Override
     public BigDecimal kilometresToMiles(BigDecimal kilometres) {
-        return null;
+        // Miles = Km / 1.609
+        return kilometres.divide(Constants.MILES_TO_KM_CONSTANT, 3, RoundingMode.HALF_EVEN);
     }
 }
